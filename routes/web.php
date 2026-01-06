2313b601f93bf0d079b224503b916191ab8a4cbc
Anthony Pauwels <anthony.pauwels.be@gmail.com>
2026-01-06 16:46:25 +0100

    clean repository

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /*
 |--------------------------------------------------------------------------
